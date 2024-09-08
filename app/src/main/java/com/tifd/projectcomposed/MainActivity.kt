ab8f525589c3661d23ac658c4ca8e30b0a9931ea
Muhammad Yasin Hakim <66106986+Y716@users.noreply.github.com>
2024-09-08 16:25:37 +0700

    Update MainActivity.kt

--- app/src/main/java/com/tifd/projectcomposed/MainActivity.kt
+++ app/src/main/java/com/tifd/projectcomposed/MainActivity.kt
@@ -7,13 +7,21 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,8 +29,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.tifd.projectcomposed.ui.theme.ProjectComposeDTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,10 +55,14 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyScreen() {
     var text by remember { mutableStateOf("") }
-    var inputText by remember { mutableStateOf("") }
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var passwordVisible by remember { mutableStateOf(false) }
+
 
     Column(
         modifier = Modifier
@@ -53,19 +71,89 @@ fun MyScreen() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = text)
-        Spacer(modifier = Modifier.height(16.dp))
-        TextField(
-            value = inputText,
-            onValueChange = { inputText = it },
-            label = { Text("Enter text") }
+        Text(
+            text = "Log in",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Your Username") },
+            shape = RoundedCornerShape(16.dp), // Rounded corners for the TextField
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color.Gray, // Color for the underline when focused
+                unfocusedBorderColor = Color.Gray // Color for the underline when unfocused
+            ),
+            modifier = Modifier.fillMaxWidth()
         )
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = {
-            text = inputText
-        }) {
-            Text("Submit")
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Your Password") },
+            shape = RoundedCornerShape(16.dp), // Rounded corners for the TextField
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color.Gray, // Color for the underline when focused
+                unfocusedBorderColor = Color.Gray // Color for the underline when unfocused
+            ),
+            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+
+            modifier = Modifier.fillMaxWidth()
+        )
+
+//                val image = if (passwordVisible)
+//                    Icons.Filled.Visibility
+//                else Icons.Filled.VisibilityOff
+//
+//                IconButton(onClick = { passwordVisible = !passwordVisible }) {
+//                    Icon(imageVector = image, contentDescription = if (passwordVisible) "Hide password" else "Show password")
+//                }
+//            }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        loginButton {
+            text = username // Set the text state when button is clicked
         }
+        Spacer(modifier = Modifier.height(16.dp))
+        FilledCardExample()
+        }
+
+    }
+
+@Composable
+fun FilledCardExample() {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        ),
+        modifier = Modifier
+            .size(width = 500.dp, height = 100.dp)
+    ) {
+        Text(
+            text = "This app is supposed to be the best app you will ever experience! But we are still underdevelopment!",
+            modifier = Modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+
+@Composable
+fun loginButton(onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF6200EE), // Background color
+                contentColor = Color.White, // Text color
+                disabledContainerColor = Color.Gray, // Background color when disabled
+            disabledContentColor = Color.LightGray // Text color when disabled
+    )
+    ) {
+        Text("Log In")
     }
 }
 
