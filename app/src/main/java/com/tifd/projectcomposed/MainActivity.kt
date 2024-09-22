2e8a6fd3afd04a08159895af89c9e8240123780a
Leafblower <ibnkhaqim@gmail.com>
2024-09-22 14:19:13 +0700

    Update M4

--- app/src/main/java/com/tifd/projectcomposed/MainActivity.kt
+++ app/src/main/java/com/tifd/projectcomposed/MainActivity.kt
@@ -1,34 +1,47 @@
 package com.tifd.projectcomposed
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerBasedShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonElevation
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItemDefaults.contentColor
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -36,16 +49,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.tifd.projectcomposed.ui.theme.ProjectComposeDTheme
 
 class MainActivity : ComponentActivity() {
@@ -64,11 +77,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun MyScreen() {
     var text by remember { mutableStateOf("") }
     var inputText by remember { mutableStateOf("") }
+    val context = LocalContext.current
+    val isFormFilled = inputText.isNotEmpty()
 
     Column(
         modifier = Modifier
@@ -97,18 +112,38 @@ fun MyScreen() {
                 label = { Text("Masukkan nama kucing")},
                 modifier = Modifier
                     .weight(1f)
-                    .fillMaxWidth(),
+                    .fillMaxWidth()
+                    .pointerInput(Unit) {
+                        detectTapGestures(
+                            onLongPress = {
+                                Toast.makeText(context, "Long Press Detected!", Toast.LENGTH_SHORT).show()
+                            }
+                        )
+                    },
                 shape = RoundedCornerShape(8.dp)
             )
         }
         Spacer(modifier = Modifier.height(16.dp))
         FilledCardExample(
-            text = "Meow meow"
+            text = "Meow meow",
         )
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = {
-            text = inputText
-        }) {
+        Button(
+            onClick = { text = inputText},
+            enabled = isFormFilled,
+            modifier = Modifier.pointerInput(Unit){
+                detectTapGestures(
+                    onLongPress = {
+                        Toast.makeText(context, "Long Press Detected!", Toast.LENGTH_SHORT).show()
+                    }
+                )
+            },
+            colors = if (isFormFilled) ButtonDefaults.buttonColors()
+            else
+                ButtonDefaults.buttonColors(
+                    containerColor = Color.LightGray
+                )
+            ) {
             Text("Meow!")
         }
 
@@ -148,6 +183,7 @@ fun LoginButton(onClick: () -> Unit) {
         Text("Log In")
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
